f9932bc5053991bace5164f896765a2e9a4babbd
Daria Leo <dashunkaleo@yandex.ru>
2026-01-28 12:17:10 +0300

    add dtos

---
+++ stats/dto/src/main/java/ru/practicum/dto/StatsRequestDto.java
@@ -0,0 +1,31 @@
+package ru.practicum.dto;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Valid
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class StatsRequestDto {
+    @NotNull
+    private String app, uri;
+
+    @NotNull
+    @Pattern(regexp = "^(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)(\\.(25[0-5]|2[0-4]\\d|1\\d\\d|[1-9]?\\d)){3}$\n",
+            message = "Принимаются только корректные адреса от 0.0.0.0 до 255.255.255.255")
+    private String ip;
+
+    @NotNull
+    @PastOrPresent
+    private LocalDateTime timestamp;
+}
